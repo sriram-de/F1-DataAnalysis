d3e6a4f1a9ca4cc293028e21a662525da540fa50
sriram-de <sriramreddy045@gmail.com>
2024-06-16 15:39:35 -0700

    Delete formula1 directory

--- formula1/formula1/includes/configuration.py
+++
@@ -1,4 +0,0 @@
-# Databricks notebook source
-raw_folder_path="/mnt/form1dl/raw"
-processed_folder_path="/mnt/form1dl/processed"
-presentation_folder_path="/mnt/form1dl/presentation"
--- formula1/formula1/trans/0.create_presentation_database.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-# MAGIC %sql
-# MAGIC CREATE DATABASE IF NOT EXISTS f1_presentation 
-# MAGIC LOCATION "/mnt/form1dl/presentation"
-
-# COMMAND ----------
-
